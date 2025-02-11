98e972e596837de1d8c9db1bf9b3444640ca5266
ABEE1003 <abioff3@gmail.com>
2025-02-11 13:58:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToShop() {
+    document.querySelector('#shop').scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+  });
